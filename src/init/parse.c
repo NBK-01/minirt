3624fb249715feb23d14c79dbbddb261a4760321
excelsior <nayefkanaan03@gmail.com>
2025-01-25 19:11:46 +0200

    placeholder function to render scene with data we parsed

--- src/init/parse.c
+++ src/init/parse.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "../../headers/main.h"
+#include "../../headers/math.h"
 #include "../../headers/minirt.h"
 
 t_color	parse_color(char *split)
@@ -159,6 +160,118 @@ int	read_file(char *filename, t_list **file)
 	close(fd);
 	return (1);
 }
+t_ray generate_ray(int x, int y, t_camera *camera) {
+    // Adjust the ray to cast from the camera through the scene
+    // Example for simple orthographic projection
+    double screen_x = (x - WIDTH / 2.0) / (WIDTH / 2.0);
+    double screen_y = (y - HEIGHT / 2.0) / (HEIGHT / 2.0);
+    
+    // The camera is looking down the negative Z axis (could adjust for perspective)
+    t_pos ray_direction = {screen_x, screen_y, -1};
+
+    // Normalize the direction vector
+    ray_direction = vec_normalize(ray_direction);
+
+    // Create a ray from the camera's position in the direction
+    t_ray ray = {camera->pos, ray_direction};
+    return ray;
+}
+
+int ray_intersects_sphere(t_ray ray, t_sphere sphere, double *t) {
+    t_pos oc = vec_operation(ray.origin, sphere.pos, SUB);
+    u_result a = vec_dot_cross(ray.dir, ray.dir, DOT);
+    u_result b = vec_dot_cross(oc, ray.dir, DOT);
+    u_result c = vec_dot_cross(oc, oc, DOT);
+	double d_b = b.d * 2.0;
+	double c_d = c.d - (sphere.diameter * sphere.diameter / 4.0);
+    double discriminant = d_b * d_b - 4.0 * a.d * c_d;
+
+    if (discriminant > 0) {
+        *t = (-b.d - sqrt(discriminant)) / (2.0 * a.d); // Closest intersection
+        return 1;
+    }
+    return 0;
+}
+
+int ray_intersects_plane(t_ray ray, t_plane plane, double *t) {
+	u_result a = vec_dot_cross(plane.vec, ray.dir, DOT);
+    double denom = a.d;
+    if (denom == 0) {
+        return 0; // Ray is parallel to the plane
+    }
+    t_pos diff = vec_operation(plane.pos, ray.origin, SUB);
+	u_result b = vec_dot_cross(diff, plane.vec, DOT);
+    *t = b.d / denom;
+    return *t >= 0; // Ensure intersection is in the right direction
+}
+
+int ray_intersects_cylinder(t_ray ray, t_cylinder cylinder, double *t) {
+    // Vector from ray origin to the cylinder's base center
+    t_pos oc = vec_operation(ray.origin, cylinder.pos, SUB);
+    
+    // The cylinder axis vector (normalized)
+    t_pos axis = vec_normalize(cylinder.vec);
+    
+    // Adjust the ray's origin and direction for cylinder's local space
+    t_pos ray_dir_proj = vec_operation(ray.dir, axis, SUB); // Project the ray direction
+    t_pos ray_origin_proj = vec_operation(oc, axis, SUB); // Project the ray origin
+
+	u_result a = vec_dot_cross(ray_dir_proj, ray_dir_proj, DOT);
+	u_result b = vec_dot_cross(ray_origin_proj, ray_dir_proj, DOT);
+	double b1 = b.d * 2.0;
+	u_result c = vec_dot_cross(ray_origin_proj, ray_origin_proj, DOT);
+    double c1 = c.d - (cylinder.diameter / 2.0) * (cylinder.diameter / 2.0);
+    double discriminant = b1 * b1 - 4.0 * a.d * c1;
+
+    if (discriminant > 0) {
+        *t = (-b1 - sqrt(discriminant)) / (2.0 * a.d);
+        return 1; // Intersection exists
+    }
+    
+    return 0; // No intersection
+}
+
+t_color calculate_lighting(t_pos intersection, t_light light, t_sphere sphere) {
+    t_pos normal = vec_normalize(vec_operation(intersection, sphere.pos, SUB)); // Normal at intersection
+    t_pos light_dir = vec_normalize(vec_operation(light.pos, intersection, SUB)); // Light direction
+    
+    // Simple diffuse shading (Lambertian shading)
+	u_result dot = vec_dot_cross(normal, light_dir, DOT);
+    double intensity = fmax(dot.d, 0.0); // Diffuse term
+    int r = (int)(sphere.color.r * intensity * light.ratio);
+    int g = (int)(sphere.color.g * intensity * light.ratio);
+    int b = (int)(sphere.color.b * intensity * light.ratio);
+    
+    return (t_color){r, g, b}; // Return the calculated color
+}
+
+void render_scene(t_mlx *mlx, t_data *data) {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+			t_ray ray = generate_ray(x, y, data->camera); // Generate a ray for this pixel
+            double t = 0;
+            int color = 0;
+
+			t_sphere sphere = (t_sphere){.pos = data->plane->pos, .diameter = data->sphere->diameter, .color = data->sphere->color};
+            // Check for intersections with the sphere
+            if (ray_intersects_sphere(ray, sphere, &t)) {
+                color = (255 << 16); // Example color for intersection with sphere (red)
+            }
+            // Check for intersections with the plane
+            else if (ray_intersects_plane(ray, (*data->plane), &t)) {
+                color = (0 << 16) | (255 << 8); // Example color for intersection with plane (green)
+            }
+            // Check for intersections with the cylinder
+            else if (ray_intersects_cylinder(ray, (*data->cylinder), &t)) {
+                color = (0 << 8); // Example color for intersection with cylinder (blue)
+            }
+            else {
+                color = 0x000000; // Background color (black if no intersection)
+            }
+			mlx_pixel_put(mlx->mlx, mlx->window, x, y, color); // Draw pixel
+        }
+    }
+}
 
 bool	parse_file(t_list **file, t_data *data)
 {
@@ -174,6 +287,14 @@ bool	parse_file(t_list **file, t_data *data)
 		parse_line(split, data);
 		tmp = tmp->next;
 	}
-	print_data(data);
+    t_mlx mlx;
+
+	mlx.mlx = mlx_init();
+    mlx.window = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "MiniRT Ray Tracing");
+    mlx.img = mlx_new_image(mlx.mlx, WIDTH, HEIGHT);
+    mlx.data = (int *)mlx_get_data_addr(mlx.img, &(int){32}, &(int){WIDTH * 4}, &(int){0});
+	/*print_data(data);*/
+	render_scene(&mlx, data);
+    mlx_loop(mlx.mlx);
 	return (true);
 }
