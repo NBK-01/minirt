9ea99f1cbba179c1e63cca47b6613aa034f38f6e
excelsior <nayefkanaan03@gmail.com>
2025-02-19 20:30:20 +0200

    mlx utility + custom pixel put

--- src/mlx/mlx.c
+++ src/mlx/mlx.c
@@ -7,3 +7,16 @@ void	rt_pixel_put(t_mlx *data, int x, int y, int color)
 	dst = data->addr + (y * data->size_line + x * (data->bpp / 8));
 	*(unsigned int*)dst = color;
 }
+
+int	close_window(int keycode, void *param)
+{
+	t_mlx	*mlx;
+
+	mlx = (t_mlx *)param;
+    if (keycode == 53)
+    {
+        mlx_destroy_window(param, mlx->window);
+        exit(0);
+    }
+    return (0);
+}
